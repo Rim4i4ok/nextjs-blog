15be1b082ee9522808d9b32f4944024b25fbf8fa
Rim4i4ok <rim4i4ok@gmail.com>
2022-10-23 18:24:45 +0300

    dummy posts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
 import { Fragment } from "react";
 
+import FeaturedPosts from "../components/home-page/featured-posts/featured-posts";
 import Hero from "../components/home-page/hero/hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "first-post",
+    title: "First Post",
+    image: "first-post.jpg",
+    excerpt:
+      "Duis consequat veniam consequat sint aliquip ipsum culpa consequat dolore sunt anim sit. Voluptate labore veniam proident laboris. Reprehenderit aliqua dolor duis sint eu laborum ex sint in tempor laborum id. Elit consequat culpa sint quis irure Lorem. Do ullamco excepteur sunt incididunt duis pariatur. Pariatur ullamco ad incididunt adipisicing.",
+    date: "2022-09-01",
+  },
+  {
+    slug: "second-post",
+    title: "Second Post",
+    image: "second-post.jpg",
+    excerpt: "Ut do reprehenderit irure excepteur ex.",
+    date: "2022-10-09",
+  },
+  {
+    slug: "thrid-post",
+    title: "Thrid Post",
+    image: "thrid-post.jpg",
+    excerpt:
+      "Adipisicing mollit consequat sint est proident officia esse cupidatat eu dolor labore dolor. Ea aute tempor ad officia consectetur nostrud proident sint velit. Adipisicing dolor consectetur eiusmod esse qui fugiat. Tempor eu voluptate ipsum nisi tempor velit ullamco excepteur sit proident eu minim dolor Lorem. Nisi culpa et velit non labore nisi quis qui nisi adipisicing laboris consequat. Pariatur dolore sint magna qui exercitation cillum proident anim officia consectetur ut quis non. Cupidatat amet quis excepteur aliqua occaecat adipisicing nisi fugiat laborum. Dolor fugiat consequat aliquip veniam tempor do pariatur exercitation duis exercitation ex eu labore minim. Labore ad do deserunt dolor quis do qui excepteur ut fugiat nostrud excepteur.",
+    date: "2022-10-12",
+  },
+  {
+    slug: "forth-post",
+    title: "Forth Post",
+    image: "forth-post.jpg",
+    excerpt: "Lorem culpa duis nisi laborum elit et.",
+    date: "2022-10-22",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
