12631af638c6e31ee04ed279ba32611a762a13f2
Rim4i4ok <rim4i4ok@gmail.com>
2022-10-23 18:32:20 +0300

    adding all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,9 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts/all-posts.";
+
+import { DUMMY_POSTS } from "../../utils/dummy-data";
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
