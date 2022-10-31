689b76c6e33edec3cd345407d54793c7996af3ae
Rim4i4ok <rim4i4ok@gmail.com>
2022-11-01 01:27:26 +0300

    rendering pretty js

--- components/post-detail/post-content/post-content.js
+++ components/post-detail/post-content/post-content.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "../post-header/post-header";
 
@@ -33,6 +35,16 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { children } = code;
+
+      return (
+        <SyntaxHighlighter language="javascript" style={atomDark}>
+          {children}
+        </SyntaxHighlighter>
+      );
     },
   };
 
---
+++ content/posts/hello-world-js.md
@@ -0,0 +1,23 @@
+---
+title: Hello World JavaScript
+excerpt: JavaScript is the most important programming language for web development.
+image: hello-world-js.png
+isFeatured: false
+date: "2021-11-01"
+---
+
+JavaScript powers the web - it's **the** most important programming language you need to know as a web developer.
+
+For example, you should understand code like this:
+
+```js
+const basics = 'Hello, World!';
+
+function printBasics() {
+  console.log(basics):
+}
+
+printBasics();
+```
+
+Learn more about it [here](https://developer.mozilla.org/en-US/docs/Web/JavaScript).
