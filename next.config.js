a54c12184d9edabfbefa943991faa12a4e29053f
Rim4i4ok <rim4i4ok@gmail.com>
2022-11-07 00:40:25 +0300

    env variables

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-  env: {
-    mongodb_username: "test-1",
-    mongodb_password: "TsZ5FghMszIvyRpP",
-    mongodb_clustername: "cluster1",
-    mongodb_database: "my-site",
-  },
+module.exports = (phase) => {
+  // development
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "test-1",
+        mongodb_password: "TsZ5FghMszIvyRpP",
+        mongodb_clustername: "cluster1",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+
+  // production
+  return {
+    env: {
+      mongodb_username: "test-1",
+      mongodb_password: "TsZ5FghMszIvyRpP",
+      mongodb_clustername: "cluster1",
+      mongodb_database: "my-site",
+    },
+  };
 };
