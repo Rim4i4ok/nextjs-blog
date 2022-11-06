a68998fad4b7ceb46a3dfac3c128fdd8751c827c
Rim4i4ok <rim4i4ok@gmail.com>
2022-11-07 00:34:50 +0300

    moving constants to config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  env: {
+    mongodb_username: "test-1",
+    mongodb_password: "TsZ5FghMszIvyRpP",
+    mongodb_clustername: "cluster1",
+    mongodb_database: "my-site",
+  },
+};
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -21,10 +21,10 @@ async function handler(req, res) {
 
     let client;
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.1dcen9j.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://test-1:TsZ5FghMszIvyRpP@cluster1.1dcen9j.mongodb.net/my-site?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database" });
       return;
