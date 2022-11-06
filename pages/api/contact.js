99e179317166d5dd8924f116d67bc96057061063
Rim4i4ok <rim4i4ok@gmail.com>
2022-11-06 20:25:28 +0300

    storring data in db

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -16,7 +18,30 @@ function handler(req, res) {
 
     // Store in db
     const newMessage = { email, name, message };
-    console.log(newMessage);
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://test-1:TsZ5FghMszIvyRpP@cluster1.1dcen9j.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.idresult = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Saving message failed" });
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({ message: "Success.", message: newMessage });
   }
